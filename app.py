9522d1dfc01de10ebfb777d1b50f91512114ee4b
Pankeel Shah <shahx317@umn.edu>
2020-01-25 20:32:49 -0600

    works now

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, Response, render_template, redirect, flash, url_for
 # import requests
 # import itertools
-# from flask_wtf.csrf import CSRFProtect
-# from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from flask_wtf import FlaskForm
 # from wtforms import StringField, SubmitField
 # import wtforms
 # from wtforms.validators import Regexp
@@ -12,10 +12,16 @@ from flask import Flask, request, Response, render_template, redirect, flash, ur
 # from models import LoginForm, SignupForm, ChampionForm, PlayerForm, SpellForm
 # from flask_sqlalchemy import SQLAlchemy
 
+csrf = CSRFProtect()
 app = Flask(__name__)
+csrf.init_app(app)
 
 
-@app.route("/")
+@app.route('/')
+def default():
+    return redirect(url_for('index'))
+
+@app.route('/index')
 def index():
-   return redirect(url_for('index'))
+    return render_template("index.html")
 
